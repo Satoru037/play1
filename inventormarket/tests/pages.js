d3d8c7acd9ef9dcb08590a663bbd78e3ae41ebfd
unknown <monishkumarms3@gmail.com>
2026-01-09 17:29:01 +0530

    Add inventormarket tests

---
+++ inventormarket/tests/pages.js
@@ -0,0 +1,16 @@
+/** @format */
+module.exports.pagesToTest = [
+	{ path: "/", name: "Home" },
+	{ path: "/about-us/", name: "About_Us" },
+	{ path: "/contact-us/", name: "Contact_Us" },
+	{ path: "/privacy-policy/", name: "Privacy_Policy" },
+	{ path: "/add-listing/", name: "Add_Listing" },
+	{ path: "/patent-services/", name: "Patent_Services" },
+	{ path: "/iump-subscription-plan/", name: "Subscription_Plans" },
+
+	// Categories
+	{ path: "/category/aviation/", name: "Cat_Aviation" },
+
+	// Explore
+	{ path: "/explore/", name: "Explore" },
+];
